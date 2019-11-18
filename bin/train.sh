fa609facbcb3630aaa56157861037a4a88c2a7c8
riku.sasaki <riku.sasaki@dena.jp>
2019-11-18 15:50:10 +0900

    update

---
+++ bin/train.sh
@@ -0,0 +1,10 @@
+# base CNN models training
+
+# 1st stacking training
+python models/first_stacking/cnn_1.py
+python models/first_stacking/cnn_2.py
+python models/first_stacking/lgbm.py
+python models/first_stacking/mlp.py
+
+# 2nd stacking training
+python models/second_stacking/lgbm.py
