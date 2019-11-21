eed8752bc0861d84d42813b24c4376303b145fa5
riku.sasaki <riku.sasaki@dena.jp>
2019-11-21 22:52:18 +0900

    update'

--- models/base_cnn/model_base/bin/predict.sh
+++ models/base_cnn/model_base/bin/predict.sh
@@ -1,13 +1,40 @@
 model=model
 gpu=0
-fold=0
 ep=2
 tta=5
 conf=./conf/${model}.py
 
+fold=0
 snapshot=../../../intermediate_output/model_base/fold${fold}_ep${ep}.pt
 valid=../../../intermediate_output/model_base/fold${fold}_valid.pkl
 test=../../../intermediate_output/model_base/fold${fold}_test.pkl
+python -m src.cnn.main test ${conf} --snapshot ${snapshot} --output ${test} --n-tta ${tta} --fold ${fold} --gpu ${gpu}
+python -m src.cnn.main valid ${conf} --snapshot ${snapshot} --output ${valid} --n-tta ${tta} --fold ${fold} --gpu ${gpu}
 
+fold=1
+snapshot=../../../intermediate_output/model_base/fold${fold}_ep${ep}.pt
+valid=../../../intermediate_output/model_base/fold${fold}_valid.pkl
+test=../../../intermediate_output/model_base/fold${fold}_test.pkl
+python -m src.cnn.main test ${conf} --snapshot ${snapshot} --output ${test} --n-tta ${tta} --fold ${fold} --gpu ${gpu}
+python -m src.cnn.main valid ${conf} --snapshot ${snapshot} --output ${valid} --n-tta ${tta} --fold ${fold} --gpu ${gpu}
+
+fold=2
+snapshot=../../../intermediate_output/model_base/fold${fold}_ep${ep}.pt
+valid=../../../intermediate_output/model_base/fold${fold}_valid.pkl
+test=../../../intermediate_output/model_base/fold${fold}_test.pkl
+python -m src.cnn.main test ${conf} --snapshot ${snapshot} --output ${test} --n-tta ${tta} --fold ${fold} --gpu ${gpu}
+python -m src.cnn.main valid ${conf} --snapshot ${snapshot} --output ${valid} --n-tta ${tta} --fold ${fold} --gpu ${gpu}
+
+fold=3
+snapshot=../../../intermediate_output/model_base/fold${fold}_ep${ep}.pt
+valid=../../../intermediate_output/model_base/fold${fold}_valid.pkl
+test=../../../intermediate_output/model_base/fold${fold}_test.pkl
+python -m src.cnn.main test ${conf} --snapshot ${snapshot} --output ${test} --n-tta ${tta} --fold ${fold} --gpu ${gpu}
+python -m src.cnn.main valid ${conf} --snapshot ${snapshot} --output ${valid} --n-tta ${tta} --fold ${fold} --gpu ${gpu}
+
+fold=4
+snapshot=../../../intermediate_output/model_base/fold${fold}_ep${ep}.pt
+valid=../../../intermediate_output/model_base/fold${fold}_valid.pkl
+test=../../../intermediate_output/model_base/fold${fold}_test.pkl
 python -m src.cnn.main test ${conf} --snapshot ${snapshot} --output ${test} --n-tta ${tta} --fold ${fold} --gpu ${gpu}
 python -m src.cnn.main valid ${conf} --snapshot ${snapshot} --output ${valid} --n-tta ${tta} --fold ${fold} --gpu ${gpu}
