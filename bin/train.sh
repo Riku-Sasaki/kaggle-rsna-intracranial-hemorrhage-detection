c2b1aef61f303440e513d348fcf37492e2127202
riku.sasaki <riku.sasaki@dena.jp>
2019-11-21 21:40:03 +0900

    update

--- bin/train.sh
+++ bin/train.sh
@@ -6,10 +6,10 @@ cd ../../../
 
 
 # 1st stacking training
-python models/first_stacking/cnn_stacking_1.py
-python models/first_stacking/cnn_stacking_2.py
-python models/first_stacking/lgbm_first_stacking.py
-python models/first_stacking/mlp_stacking.py
+#python models/first_stacking/cnn_stacking_1.py
+#python models/first_stacking/cnn_stacking_2.py
+#python models/first_stacking/lgbm_first_stacking.py
+#python models/first_stacking/mlp_stacking.py
 
 # 2nd stacking training
-python models/second_stacking/lgbm_second_stacking.py
+#python models/second_stacking/lgbm_second_stacking.py
