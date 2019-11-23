6f736cb95240b63790fe499608cfc7139163109b
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-11-23 19:44:43 +0000

    tnp

---
+++ models/base_cnn/sugawara_efficientnetb3/bin/train.sh
@@ -0,0 +1,9 @@
+model=sugawara_efficientnetb3
+gpu=0
+conf=./conf/${model}.py
+
+python -m src.cnn.main train ${conf} --fold 0 --gpu ${gpu}
+python -m src.cnn.main train ${conf} --fold 1 --gpu ${gpu}
+python -m src.cnn.main train ${conf} --fold 2 --gpu ${gpu}
+python -m src.cnn.main train ${conf} --fold 3 --gpu ${gpu}
+python -m src.cnn.main train ${conf} --fold 4 --gpu ${gpu}
