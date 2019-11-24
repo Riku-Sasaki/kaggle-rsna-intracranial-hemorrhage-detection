0dc4c362913910fe76a70b528bdaa071d63be766
shimacos37 <naoto.shimakoshi@gmail.com>
2019-11-25 08:15:00 +0900

    refactoring

--- models/second_stacking/bin/stacking_cmd.sh
+++ models/second_stacking/bin/stacking_cmd.sh
@@ -2,4 +2,4 @@
 
 python preprocess_for_sugawara_stacking2.py
 python get_feats_for_2nd_stacking.py
-# python lgbm_second_stacking.py
+python lgbm_second_stacking.py
--- models/second_stacking/bin/submission.sh
+++ models/second_stacking/bin/submission.sh
@@ -6,6 +6,7 @@ clip=1e-6
 docker run --rm \
     -v $PWD/:/root/ \
     -v $PWD/../../intermediate_output/:/root/intermediate_output/ \
+    -v $PWD/../../input/:/root/input/ \
     -v $PWD/../../output/:/root/output/ \
     -v $HOME/.cache/:/root/.cache \
     --runtime=nvidia \
